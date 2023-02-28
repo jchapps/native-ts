b31afd4fe582e45c336fbd0f7c30976f7130c83f
Josh Chappelow <jdchappelow@gmail.com>
2023-02-28 20:28:15 +0900

    build: react native navigation

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from "react-native"
+type Props = {}
+
+const HomeScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>
+        Homescreen
+      </Text>
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ screens/PlannerScreen.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from "react-native"
+type Props = {}
+
+const PlannerScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>
+        Plannerscreen
+      </Text>
+    </View>
+  )
+}
+
+export default PlannerScreen
