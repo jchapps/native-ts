61b1c41914be17433a613ddd79c8a9a1d48a788e
Josh Chappelow <jdchappelow@gmail.com>
2023-03-02 13:05:50 +0900

    feat: useisfocused to fetch data when revisiting homepage

--- hooks/useWorkouts.ts
+++ hooks/useWorkouts.ts
@@ -2,16 +2,25 @@ import { useEffect, useState } from "react";
 import { getWorkouts } from "../storage/workout";
 import { Workout } from "../types/data";
 
+import { useIsFocused } from "@react-navigation/native";
+
 export const useWorkouts = () => {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const isFocused = useIsFocused();
+
+  // console.log(isFocused) - returns false when on a different page, true on home
 
   useEffect(() => {
-    async function getData() { //GETTING DATA
+    async function getData() {//GETTING DATA
+      console.log("Getting data from storage");
       const _workouts = await getWorkouts(); // GETTING ARRAY OF OBJECTS
       setWorkouts(_workouts); // UPDATE STATE
     }
-    getData();
-  }, []);
 
-  return workouts;
+    if (isFocused) {
+      getData();
+    }
+  }, [isFocused]);
+
+  return workouts; // initially returns empty array until useEffect fetchs workouts
 };
