ec48f72b5bcdfc07b60e2516d1ad37d1e89b0c7b
Josh Chappelow <jdchappelow@gmail.com>
2023-03-06 22:04:02 +0900

    fix: timer reaches 0 cleanup function

--- hooks/useCountDown.ts
+++ hooks/useCountDown.ts
@@ -1,25 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function useCountDown(idx: number, initialCount: number) {
   const [countDown, setCountDown] = useState(initialCount);
+  const intervalRef = useRef<number>() // we don't want to lose the current number when component remounts
 
   useEffect(() => {
     if (idx == -1) {
       return;
     } // don't update on open - only when it's updated
-    const intervalId = window.setInterval(() => {
+    intervalRef.current = window.setInterval(() => {
       setCountDown((count) => {
         console.log(count);
         return count - 1;
       });
-    }, 1000); // runs every second
+    }, 50); // runs every second
 
-    return () => window.clearInterval(intervalId); // runs on dismount - cleanup function
+    return cleanup; // runs on dismount - cleanup function
   }, [idx]);
 
   useEffect(() => {
     setCountDown(initialCount)
   }, [initialCount]);
+
+  useEffect(() => {
+    if (countDown === 0) {
+      cleanup()
+    }
+  }, [countDown])
+
+  const cleanup = () => {
+    if (intervalRef.current) {
+      window.clearInterval(intervalRef.current)
+      intervalRef.current = undefined
+    }
+
+  }
 
   return countDown;
 }
